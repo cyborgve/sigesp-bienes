96518e534bc16a2bac2e1e563db8c62491e49824
Richard Iribarren <richardiribarren@gmail.com>
2023-07-14 02:09:43 -0400

    fix: chang moment-js locale to angular/core locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/app/material/material.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
 import { AppModule as SigespModule } from 'sigesp';
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-} from '@angular/material/core';
-import {
-  MAT_MOMENT_DATE_FORMATS,
-  MomentDateAdapter,
-} from '@angular/material-moment-adapter';
 import { HeaderHttpInterceptor } from '@core/interceptors/header-http.interceptor';
+import { registerLocaleData } from '@angular/common';
+import esVE from '@angular/common/locales/es-VE';
+
+registerLocaleData(esVE, 'es-VE');
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,17 +20,10 @@ import { HeaderHttpInterceptor } from '@core/interceptors/header-http.intercepto
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    ToastrModule.forRoot(),
     SigespModule,
   ],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'es-Ve' },
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE],
-    },
-    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    { provide: LOCALE_ID, useValue: 'es-VE' },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HeaderHttpInterceptor,
