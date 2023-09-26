436f67bcfd18cce118a43ee84d81072436907e1c
Richard Iribarren <richardiribarren@gmail.com>
2023-09-26 10:10:30 -0400

    chore: se agrega filtrar por tipo

--- src/app/core/services/definiciones/causa-movimiento.service.ts
+++ src/app/core/services/definiciones/causa-movimiento.service.ts
@@ -1,7 +1,10 @@
+import { map } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { GenericService } from '@core/services/auxiliares/generic.service';
 import { CausaMovimiento } from '@core/models/definiciones/causa-movimiento';
 import { END_POINTS } from '@core/constants/end-points';
+import { TipoCausaMovimiento } from '@core/types/tipo-causa-movimiento';
 
 @Injectable({
   providedIn: 'root',
@@ -10,4 +13,14 @@ export class CausaMovimientoService extends GenericService<CausaMovimiento> {
   protected getEntidadUrl(): string {
     return END_POINTS.find(ep => ep.clave === 'causaMovimiento').valor;
   }
+
+  filtrarPorTipo = (tipoCausaMovimiento: TipoCausaMovimiento) =>
+    pipe(
+      map((causasMovimiento: CausaMovimiento[]) =>
+        causasMovimiento.filter(
+          causaMovimiento =>
+            causaMovimiento.tipo === tipoCausaMovimiento.substring(0, 1)
+        )
+      )
+    );
 }
