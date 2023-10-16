fa220f0a8e6efec428d8c451290bedb52cb0db4d
Richard Iribarren <richardiribarren@gmail.com>
2023-10-16 04:14:27 -0400

    chore: actualizacion de reporte

--- src/app/pages/reportes/inventario-activos/inventario-activos.component.ts
+++ src/app/pages/reportes/inventario-activos/inventario-activos.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-inventario-activos',
   templateUrl: './inventario-activos.component.html',
   styleUrls: ['./inventario-activos.component.scss'],
 })
-export class InventarioActivosComponent {}
+export class InventarioActivosComponent {
+  titulo = 'Reportes: Inventario de Bienes';
+  fechaEmision = new Date();
+  formularioRangoFechas: FormGroup;
+}
--- src/app/pages/reportes/inventario-activos/inventario-activos.module.ts
+++ src/app/pages/reportes/inventario-activos/inventario-activos.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { InventarioActivosRoutingModule } from './inventario-activos-routing.module';
 import { InventarioActivosComponent } from './inventario-activos.component';
+import { MatCardModule } from '@angular/material/card';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [InventarioActivosComponent],
-  imports: [CommonModule, InventarioActivosRoutingModule],
+  imports: [
+    CommonModule,
+    InventarioActivosRoutingModule,
+    MatCardModule,
+    SharedModule,
+  ],
 })
 export class InventarioActivosModule {}
