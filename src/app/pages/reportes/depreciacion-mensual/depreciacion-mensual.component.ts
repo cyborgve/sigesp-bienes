05d5044b8f645629ed4d409e3e2586ad6cf6e732
Richard Iribarren <richardiribarren@gmail.com>
2023-10-16 04:11:59 -0400

    chore: reporte depreciaciones mensuales

--- src/app/pages/reportes/depreciacion-mensual/depreciacion-mensual.component.ts
+++ src/app/pages/reportes/depreciacion-mensual/depreciacion-mensual.component.ts
@@ -1,8 +1,60 @@
+import { map, tap, take } from 'rxjs/operators';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DepreciacionDetalleService } from '@core/services/procesos/depreciacion-detalle.service';
+import moment from 'moment';
+import { MatTableDataSource } from '@angular/material/table';
+import { DetalleDepreciacion } from '@core/models/procesos/detalle-depreciacion';
 
 @Component({
   selector: 'app-depreciacion-mensual',
   templateUrl: './depreciacion-mensual.component.html',
   styleUrls: ['./depreciacion-mensual.component.scss'],
 })
-export class DepreciacionMensualComponent {}
+export class DepreciacionMensualComponent {
+  titulo = 'Reportes: Depreciaciones Mensuales';
+  fechaEmision = new Date();
+  formularioRangoFechas: FormGroup;
+  dataSource: MatTableDataSource<DetalleDepreciacion> =
+    new MatTableDataSource();
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _depreciacionDetalle: DepreciacionDetalleService
+  ) {
+    this.formularioRangoFechas = this._formBuilder.group({
+      rango: ['TODOS'],
+      fechaInicio: [undefined],
+      fechaFin: [undefined],
+      fechaReferencia: ['CREADO'],
+    });
+    this.recargarDatos();
+  }
+
+  private recargarDatos() {
+    let fechaInicial = this.formularioRangoFechas.value.fechaInicio
+      ? this.formularioRangoFechas.value.fechaInicio
+      : new Date(1);
+    let fechaFinal = this.formularioRangoFechas.value.fechaFin
+      ? this.formularioRangoFechas.value.fechaFin
+      : new Date();
+    let inicio = moment(fechaInicial);
+    let fin = moment(fechaFinal);
+    this._depreciacionDetalle
+      .buscarTodos()
+      .pipe(
+        map(detalles =>
+          detalles.filter(detalle =>
+            moment(detalle.fecha).isBetween(inicio, fin)
+          )
+        ),
+        tap(detalles => {
+          this.dataSource = new MatTableDataSource(detalles);
+        }),
+        take(1)
+      )
+      .subscribe();
+  }
+
+  guardar() {}
+}
--- src/app/pages/reportes/depreciacion-mensual/depreciacion-mensual.module.ts
+++ src/app/pages/reportes/depreciacion-mensual/depreciacion-mensual.module.ts
@@ -3,9 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { DepreciacionMensualRoutingModule } from './depreciacion-mensual-routing.module';
 import { DepreciacionMensualComponent } from './depreciacion-mensual.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FiltrosReportesModule } from '../filtros-reportes/filtros-reportes.module';
+import { TablaDepreciacionModule } from '@pages/procesos/depreciaciones/tabla-depreciacion/tabla-depreciacion.module';
+import { SharedModule } from '@shared/shared.module';
+import { DetalleComponent } from './detalle/detalle.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [DepreciacionMensualComponent],
-  imports: [CommonModule, DepreciacionMensualRoutingModule],
+  declarations: [DepreciacionMensualComponent, DetalleComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    FiltrosReportesModule,
+    TablaDepreciacionModule,
+    DepreciacionMensualRoutingModule,
+    MatCardModule,
+    MatTableModule,
+  ],
 })
 export class DepreciacionMensualModule {}
