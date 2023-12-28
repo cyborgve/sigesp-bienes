4f710e443e5ee4de53605f31958404990cef65ab
Richard Iribarren <richardiribarren@gmail.com>
2023-12-28 05:38:44 -0400

    chore(entrega de unidad): actualizacion de proceso

--- src/app/core/utils/pipes-rxjs/procesos/ejecutar-entrega-unidad.ts
+++ src/app/core/utils/pipes-rxjs/procesos/ejecutar-entrega-unidad.ts
@@ -1,10 +1,12 @@
 import { EntregaUnidad } from '@core/models/procesos/entrega-unidad';
 import { switchMap, map } from 'rxjs/operators';
-import { pipe } from 'rxjs';
+import { forkJoin, pipe } from 'rxjs';
 import { UnidadAdministrativaService } from '@core/services/definiciones/unidad-administrativa.service';
+import { ActivoUbicacionService } from '@core/services/definiciones/activo-ubicacion.service';
 
 export const ejecutarEntregaUnidad = (
-  _unidadAdministrativa: UnidadAdministrativaService
+  _unidadAdministrativa: UnidadAdministrativaService,
+  _activoUbicacion: ActivoUbicacionService
 ) =>
   pipe(
     switchMap((entregaUnidad: EntregaUnidad) => {
@@ -23,5 +25,28 @@ export const ejecutarEntregaUnidad = (
               .pipe(map(() => entregaUnidad));
           })
         );
+    }),
+    switchMap(entregaUnidad => {
+      let buscarUbicaciones = entregaUnidad.activos.map(activoProceso =>
+        _activoUbicacion.buscarPorId(activoProceso.activo).pipe(
+          map(activoUbicacion => {
+            activoUbicacion.responsableId = entregaUnidad.nuevoResponsable;
+            return activoUbicacion;
+          })
+        )
+      );
+      return forkJoin(buscarUbicaciones).pipe(
+        switchMap(ubicacionesEncontradas => {
+          let actualizarUbicaciones = ubicacionesEncontradas.map(ubicacion =>
+            _activoUbicacion.actualizar(
+              ubicacion.id,
+              ubicacion,
+              undefined,
+              false
+            )
+          );
+          return forkJoin(actualizarUbicaciones).pipe(map(() => entregaUnidad));
+        })
+      );
     })
   );
--- src/app/core/utils/pipes-rxjs/procesos/reversar-entrega-unidad.ts
+++ src/app/core/utils/pipes-rxjs/procesos/reversar-entrega-unidad.ts
@@ -1,6 +1,54 @@
-import { EntregaUnidadService } from '@core/services/procesos/entrega-unidad.service';
-import { pipe } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { ActivoUbicacion } from '@core/models/definiciones/activo-ubicacion';
+import { UnidadAdministrativa } from '@core/models/definiciones/unidad-administrativa';
+import { EntregaUnidad } from '@core/models/procesos/entrega-unidad';
+import { ActivoUbicacionService } from '@core/services/definiciones/activo-ubicacion.service';
+import { UnidadAdministrativaService } from '@core/services/definiciones/unidad-administrativa.service';
+import { forkJoin, pipe } from 'rxjs';
+import { filter, switchMap, map } from 'rxjs/operators';
 
-export const reversarEntregaUnidad = (_entregaUnidad: EntregaUnidadService) =>
-  pipe(tap());
+export const reversarEntregaUnidad = (
+  entregaUnidad: EntregaUnidad,
+  _unidadAdministrativa: UnidadAdministrativaService,
+  _activoUbicacion: ActivoUbicacionService
+) =>
+  pipe(
+    filter((eliminado: boolean) => eliminado),
+    switchMap(() => {
+      let buscarUnidadAdministrativa = _unidadAdministrativa
+        .buscarPorId(entregaUnidad.unidadAdministrativa)
+        .pipe(
+          map(unidadAdministrativa => {
+            unidadAdministrativa.responsable =
+              entregaUnidad.responsableAnterior;
+            return unidadAdministrativa;
+          })
+        );
+      let buscarUbicacionActivos = entregaUnidad.activos.map(activoProceso =>
+        _activoUbicacion.buscarPorId(activoProceso.activo).pipe(
+          map(ubicacion => {
+            ubicacion.responsableId = entregaUnidad.responsableAnterior;
+            return ubicacion;
+          })
+        )
+      );
+      return forkJoin([
+        buscarUnidadAdministrativa,
+        ...buscarUbicacionActivos,
+      ]).pipe(
+        switchMap(encontrados => {
+          let unidadAdministrativa = encontrados[0] as UnidadAdministrativa;
+          let activosUbicacion = encontrados.slice(1) as ActivoUbicacion[];
+          let actualizarUnidadAdministrativa = _unidadAdministrativa.actualizar(
+            unidadAdministrativa.id,
+            unidadAdministrativa,
+            undefined,
+            false
+          );
+          return forkJoin([
+            actualizarUnidadAdministrativa,
+            ...activosUbicacion,
+          ]);
+        })
+      );
+    })
+  );
